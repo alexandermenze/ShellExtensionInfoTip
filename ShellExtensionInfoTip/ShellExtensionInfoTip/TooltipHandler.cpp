7d0ed362726bfeac960d8b5a2d5d314177c48192
Alexander Menze <a.menze@polipol.de>
2018-02-14 17:23:28 +0100

    Created empty shell extension tooltip handler

---
+++ ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.cpp
@@ -0,0 +1,1 @@
+#include "TooltipHandler.h"
---
+++ ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.h
@@ -0,0 +1,12 @@
+#ifndef _TOOLTIP_HANDLER_H
+
+#define _TOOLTIP_HANDLER_H
+
+#include <Windows.h>
+#include <ShlObj.h>
+
+class TooltipHandler : IQueryInfo {
+
+};
+
+#endif // !_TOOLTIP_HANDLER_H
