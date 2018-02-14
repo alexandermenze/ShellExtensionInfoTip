15f5f35380d226a5d98a92bd481f434c83ecebdd
Alexander Menze <a.menze@polipol.de>
2018-02-14 18:07:34 +0100

    Added debug tooltip message

--- ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.cpp
+++ ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.cpp
@@ -1,1 +1,17 @@
 #include "TooltipHandler.h"
+#include "Shlobj.h"
+#include <string>
+
+HRESULT TooltipHandler::GetInfoFlags(DWORD* pdwFlags)
+{
+	return E_NOTIMPL;
+}
+
+HRESULT TooltipHandler::GetInfoTip(DWORD dwFlags, PWSTR* ppwszTip)
+{
+	std::wstring tooltipText(L"This is my custom addon!");
+	PWSTR* stringPointer = (PWSTR*)CoTaskMemAlloc(tooltipText.length() + 1);
+	wcscpy((WCHAR*)stringPointer, tooltipText.c_str());
+	ppwszTip = stringPointer;
+	return S_OK;
+}
--- ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.h
+++ ShellExtensionInfoTip/ShellExtensionInfoTip/TooltipHandler.h
@@ -6,7 +6,8 @@
 #include <ShlObj.h>
 
 class TooltipHandler : IQueryInfo {
-
+	COM_DECLSPEC_NOTHROW HRESULT STDMETHODCALLTYPE GetInfoFlags(DWORD *pdwFlags) override;
+	COM_DECLSPEC_NOTHROW HRESULT STDMETHODCALLTYPE GetInfoTip(THIS_ DWORD dwFlags, _Outptr_ PWSTR *ppwszTip) override;
 };
 
 #endif // !_TOOLTIP_HANDLER_H
